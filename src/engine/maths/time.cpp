0011511f5612a9d4291f5d85d9baec218f27223e
_ <_>
2020-10-24 13:49:46 +0200

    feat(engine/maths): add time container

---
+++ src/engine/maths/time.cpp
@@ -0,0 +1,192 @@
+#include "time.hpp"
+
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+
+namespace engine::maths {
+    Time Time::now()
+    {
+        static const auto localEpoch = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::high_resolution_clock::now() - localEpoch);
+    }
+
+    std::string Time::getDateTime(const std::string& format)
+    {
+        auto now = std::chrono::system_clock::now();
+        auto timeT = std::chrono::system_clock::to_time_t(now);
+
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&timeT), format.c_str());
+        return ss.str();
+    }
+
+    constexpr bool Time::operator==(const Time& rhs) const
+    {
+        return m_value == rhs.m_value;
+    }
+
+    constexpr bool Time::operator!=(const Time& rhs) const
+    {
+        return m_value != rhs.m_value;
+    }
+
+    constexpr bool Time::operator<(const Time& rhs) const
+    {
+        return m_value < rhs.m_value;
+    }
+
+    constexpr bool Time::operator<=(const Time& rhs) const
+    {
+        return m_value <= rhs.m_value;
+    }
+
+    constexpr bool Time::operator>(const Time& rhs) const
+    {
+        return m_value > rhs.m_value;
+    }
+
+    constexpr bool Time::operator>=(const Time& rhs) const
+    {
+        return m_value >= rhs.m_value;
+    }
+
+    constexpr Time Time::operator-() const
+    {
+        return Time(-m_value);
+    }
+
+    constexpr Time& Time::operator+=(const Time& rhs)
+    {
+        return *this = *this + rhs;
+    }
+
+    constexpr Time& Time::operator-=(const Time& rhs)
+    {
+        return *this = *this - rhs;
+    }
+
+    constexpr Time& Time::operator*=(float rhs)
+    {
+        return *this = *this * rhs;
+    }
+
+    constexpr Time& Time::operator*=(int64_t rhs)
+    {
+        return *this = *this * rhs;
+    }
+
+    constexpr Time& Time::operator/=(float rhs)
+    {
+        return *this = *this / rhs;
+    }
+
+    constexpr Time& Time::operator/=(int64_t rhs)
+    {
+        return *this = *this / rhs;
+    }
+
+    constexpr Time operator+(const Time& lhs, const Time& rhs)
+    {
+        return lhs.m_value + rhs.m_value;
+    }
+
+    constexpr Time operator-(const Time& lhs, const Time& rhs)
+    {
+        return lhs.m_value - rhs.m_value;
+    }
+
+    constexpr Time operator*(const Time& lhs, float rhs)
+    {
+        return lhs.m_value * rhs;
+    }
+
+    constexpr Time operator*(const Time& lhs, int64_t rhs)
+    {
+        return lhs.m_value * rhs;
+    }
+
+    constexpr Time operator*(float lhs, const Time& rhs)
+    {
+        return lhs * rhs.m_value;
+    }
+
+    constexpr Time operator*(int64_t lhs, const Time& rhs)
+    {
+        return lhs * rhs.m_value;
+    }
+
+    constexpr Time operator/(const Time& lhs, float rhs)
+    {
+        return lhs.m_value / rhs;
+    }
+
+    constexpr Time operator/(const Time& lhs, int64_t rhs)
+    {
+        return lhs.m_value / rhs;
+    }
+
+    constexpr double operator/(const Time& lhs, const Time& rhs)
+    {
+        return lhs.m_value / rhs.m_value;
+    }
+
+    ElapsedTime::ElapsedTime(const Time& interval)
+        : m_startTime(Time::now())
+        , m_interval(interval)
+    {
+    }
+
+    const Time& ElapsedTime::getStartTime() const
+    {
+        return m_startTime;
+    }
+
+    void ElapsedTime::setStartTime(const Time& startTime)
+    {
+        m_startTime = startTime;
+    }
+
+    const Time& ElapsedTime::getInterval() const
+    {
+        return m_interval;
+    }
+
+    void ElapsedTime::setInterval(const Time& interval)
+    {
+        m_interval = interval;
+    }
+
+    uint32_t ElapsedTime::getElapsed()
+    {
+        auto now = Time::now();
+        auto elapsed =
+            static_cast<uint32_t>(std::floor((now - m_startTime) / m_interval));
+
+        if (elapsed != 0.0f) {
+            m_startTime = now;
+        }
+
+        return elapsed;
+    }
+
+    void Delta::update()
+    {
+        currentTime = Time::now();
+        change = currentTime - lastTime;
+        lastTime = currentTime;
+    }
+
+    void ChangePerSecond::update(const Time& time)
+    {
+        valueTemp++;
+
+        if (std::floor(time.asSeconds()) > std::floor(valueTime.asSeconds())) {
+            value = valueTemp;
+            valueTemp = 0;
+        }
+
+        valueTime = time;
+    }
+} // namespace engine::maths
---
+++ src/engine/maths/time.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+
+namespace engine::maths {
+    class Time {
+      public:
+        template <typename Rep, typename Period>
+        constexpr Time(const std::chrono::duration<Rep, Period>& duration)
+            : m_value(
+                  std::chrono::duration_cast<std::chrono::microseconds>(duration).count())
+        {
+        }
+
+        template <typename Rep = float>
+        static constexpr Time seconds(const Rep& seconds)
+        {
+            return Time(std::chrono::duration<Rep>(seconds));
+        }
+
+        template <typename Rep = int32_t>
+        static constexpr Time milliSeconds(const Rep& milliSeconds)
+        {
+            return Time(std::chrono::duration<Rep>(milliSeconds));
+        }
+
+        template <typename Rep = int64_t>
+        static constexpr Time microSeconds(const Rep& microSeconds)
+        {
+            return Time(std::chrono::duration<Rep>(microSeconds));
+        }
+
+        template <typename T = float>
+        constexpr auto asSeconds() const
+        {
+            return static_cast<T>(m_value.count() / static_cast<T>(1000000));
+        }
+
+        template <typename T = uint32_t>
+        constexpr auto asMilliSeconds() const
+        {
+            return static_cast<T>(m_value.count() / static_cast<T>(1000));
+        }
+
+        template <typename T = uint64_t>
+        constexpr auto asMicroSeconds() const
+        {
+            return static_cast<T>(m_value.count());
+        }
+
+        static Time now();
+
+        static std::string getDateTime(const std::string& format = "%H:%M:%S %d-%m-%Y");
+
+        template <typename Rep, typename Period>
+        constexpr operator std::chrono::duration<Rep, Period>() const
+        {
+            return std::chrono::duration_cast<std::chrono::duration<Rep, Period>>(
+                m_value);
+        }
+
+        constexpr bool operator==(const Time& rhs) const;
+        constexpr bool operator!=(const Time& rhs) const;
+        constexpr bool operator<(const Time& rhs) const;
+        constexpr bool operator<=(const Time& rhs) const;
+        constexpr bool operator>(const Time& rhs) const;
+        constexpr bool operator>=(const Time& rhs) const;
+
+        constexpr Time operator-() const;
+
+        friend constexpr Time operator+(const Time& lhs, const Time& rhs);
+        friend constexpr Time operator-(const Time& lhs, const Time& rhs);
+        friend constexpr Time operator*(const Time& lhs, float rhs);
+        friend constexpr Time operator*(const Time& lhs, int64_t rhs);
+        friend constexpr Time operator*(float lhs, const Time& rhs);
+        friend constexpr Time operator*(int64_t lhs, const Time& rhs);
+        friend constexpr Time operator/(const Time& lhs, float rhs);
+        friend constexpr Time operator/(const Time& lhs, int64_t rhs);
+        friend constexpr double operator/(const Time& lhs, const Time& rhs);
+
+        constexpr Time& operator+=(const Time& rhs);
+        constexpr Time& operator-=(const Time& rhs);
+        constexpr Time& operator*=(float rhs);
+        constexpr Time& operator*=(int64_t rhs);
+        constexpr Time& operator/=(float rhs);
+        constexpr Time& operator/=(int64_t rhs);
+
+      private:
+        std::chrono::microseconds m_value{};
+    };
+
+    class ElapsedTime {
+      public:
+        ElapsedTime(const Time& interval = Time::seconds(-1));
+
+        [[nodiscard]] const Time& getStartTime() const;
+
+        void setStartTime(const Time& startTime);
+
+        [[nodiscard]] const Time& getInterval() const;
+
+        void setInterval(const Time& interval);
+
+        uint32_t getElapsed();
+
+      private:
+        Time m_startTime;
+        Time m_interval;
+    };
+
+    class Delta {
+      public:
+        void update();
+
+        Time currentTime;
+        Time lastTime;
+        Time change;
+    };
+
+    class ChangePerSecond {
+      public:
+        void update(const Time& time);
+
+        uint32_t valueTemp = 0;
+        uint32_t value = 0;
+        Time valueTime;
+    };
+} // namespace engine::maths
