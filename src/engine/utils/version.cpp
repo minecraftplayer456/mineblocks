1ca33b78351a44927e218ecb3edbd4a66d2adac2
_ <_>
2020-10-24 19:17:02 +0200

    refactor(engine/utils): add stringify to version.cpp

--- src/engine/utils/version.cpp
+++ src/engine/utils/version.cpp
@@ -1,5 +1,7 @@
 #include "version.hpp"
 
+#include <vulkan/vulkan_core.h>
+
 namespace engine::utils {
     Version::Version(int major, int minor, int patch)
         : m_major(major)
@@ -21,5 +23,16 @@ namespace engine::utils {
     int Version::getPatch() const
     {
         return m_patch;
+    }
+
+    std::string Version::toString() const
+    {
+        return std::to_string(m_major) + "." + std::to_string(m_minor) + "." +
+               std::to_string(m_patch);
+    }
+
+    uint32_t Version::toVulkanVersion() const
+    {
+        return VK_MAKE_VERSION(m_major, m_minor, m_patch);
     }
 } // namespace engine::utils
--- src/engine/utils/version.hpp
+++ src/engine/utils/version.hpp
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <string>
+
 namespace engine::utils {
     class Version {
       public:
         Version(int major, int minor, int patch);
 
-        int getMajor() const;
-        int getMinor() const;
-        int getPatch() const;
+        [[nodiscard]] int getMajor() const;
+        [[nodiscard]] int getMinor() const;
+        [[nodiscard]] int getPatch() const;
+
+        [[nodiscard]] std::string toString() const;
+        [[nodiscard]] uint32_t toVulkanVersion() const;
 
       private:
         int m_major;
