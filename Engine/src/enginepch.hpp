47916dca579d70fb2a9a5d54e85c694fe03cf809
_ <_>
2020-11-01 14:39:24 +0100

    feat(engine): add precompiled headers

---
+++ Engine/src/enginepch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <string>
+
+#include <array>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
