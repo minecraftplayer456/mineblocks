d2350408874594f0ff3fd0f86b77e9d12b3bc554
_ <_>
2020-11-02 10:29:36 +0100

    feat(engine/core): add logging

---
+++ Engine/src/Engine/Core/Log.cpp
@@ -0,0 +1,27 @@
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+namespace Engine {
+    Ref<spdlog::logger> Log::s_coreLogger;
+    Ref<spdlog::logger> Log::s_clientLogger;
+
+    void Log::Init()
+    {
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(
+            std::make_shared<spdlog::sinks::basic_file_sink_mt>("Game.log", true));
+
+        s_coreLogger =
+            std::make_shared<spdlog::logger>("ENGINE", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_coreLogger);
+        s_coreLogger->set_level(spdlog::level::trace);
+        s_coreLogger->flush_on(spdlog::level::trace);
+
+        s_clientLogger =
+            std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_clientLogger);
+        s_clientLogger->set_level(spdlog::level::trace);
+        s_clientLogger->flush_on(spdlog::level::trace);
+    }
+} // namespace Engine
---
+++ Engine/src/Engine/Core/Log.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+namespace Engine {
+    class Log {
+      public:
+        static void Init();
+
+        static Ref<spdlog::logger>& GetCoreLogger()
+        {
+            return s_coreLogger;
+        }
+        static Ref<spdlog::logger>& GetClientLogger()
+        {
+            return s_clientLogger;
+        }
+
+      private:
+        static Ref<spdlog::logger> s_coreLogger;
+        static Ref<spdlog::logger> s_clientLogger;
+    };
+} // namespace Engine
+
+#define ENGINE_CORE_TRACE(...) ::Engine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ENGINE_CORE_DEBUG(...) ::Engine::Log::GetCoreLogger()->debug(__VA_ARGS__)
+#define ENGINE_CORE_INFO(...) ::Engine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ENGINE_CORE_WARN(...) ::Engine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ENGINE_CORE_ERROR(...) ::Engine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ENGINE_CORE_CRITICAL(...) ::Engine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define ENGINE_TRACE(...) ::Engine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ENGINE_DEBUG(...) ::Engine::Log::GetClientLogger()->debug(__VA_ARGS__)
+#define ENGINE_INFO(...) ::Engine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ENGINE_WARN(...) ::Engine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ENGINE_ERROR(...) ::Engine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ENGINE_CRITICAL(...) ::Engine::Log::GetClientLogger()->critical(__VA_ARGS__)
