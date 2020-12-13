71828f115c3b0a65997f3848d5a641d3733f90ad
_ <_>
2020-12-13 16:41:47 +0100

    chore(all): setup project v2

---
+++ Engine/src/EnginePch.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <algorithm>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include <array>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
