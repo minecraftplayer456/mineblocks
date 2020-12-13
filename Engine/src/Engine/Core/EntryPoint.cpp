c949088137faca15ddd5935785ed23c11770c0ae
_ <_>
2020-12-13 16:48:10 +0100

    Remove everything for rewrite

--- Engine/src/Engine/Core/EntryPoint.cpp
+++
@@ -1,8 +0,0 @@
-#include "EntryPoint.hpp"
-
-auto main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) -> int
-{
-    auto engine = Engine::Engine(Engine::CreateApplication());
-
-    return engine.Run();
-}
--- Engine/src/Engine/Core/EntryPoint.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Engine/Core/Engine.hpp"
-
-extern auto Engine::CreateApplication() -> Application*;
