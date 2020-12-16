1d588dfc64ff6b2104a189bfbc4672232a3bff44
minecraftplayer456 <marek.sengbusch@flg-online.de>
2020-12-16 17:45:02 +0100

    feat(all): Start over with new cmake project

---
+++ src/Pch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <array>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
