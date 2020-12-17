7547dce1b663f90eb6bce35c4bc51107bdb8849c
minecraftplayer456 <marek.sengbusch@flg-online.de>
2020-12-17 14:49:19 +0100

    feat(common): upgrade logging system

--- src/Mineblocks/Common/Log.cpp
+++ src/Mineblocks/Common/Log.cpp
@@ -3,34 +3,9 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-namespace Mineblocks{
-    void Log::Initialize()
+namespace Mineblocks {
+    auto LogCategoryToString(LogCategory category) -> const char*
     {
-        std::vector<spdlog::sink_ptr> logSinks;
-
-        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_st>());
-        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_st>("Game.log", true));
-
-        commonLogger = std::make_shared<spdlog::logger>("Common", std::begin(logSinks), std::end(logSinks));
-        coreLogger = std::make_shared<spdlog::logger>("Core", std::begin(logSinks), std::end(logSinks));
-
-        spdlog::register_logger(commonLogger);
-        spdlog::register_logger(coreLogger);
-
-        commonLogger->set_level(spdlog::level::trace);
-        coreLogger->set_level(spdlog::level::trace);
-    }
-
-    auto Log::Common() -> std::shared_ptr<spdlog::logger>
-    {
-        return commonLogger;
-    }
-    auto Log::Core() -> std::shared_ptr<spdlog::logger>
-    {
-        return coreLogger;
-    }
-
-    auto Log::LogCategoryToString(LogCategory category) -> const char* {
         switch (category) {
             case LogCategory::Common:
                 return "Common";
@@ -39,4 +14,27 @@ namespace Mineblocks{
         }
         return "Unknown-Category";
     }
-}
+
+    Logger::Logger()
+    {
+        std::vector<spdlog::sink_ptr> logSinks;
+
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_st>());
+        logSinks.emplace_back(
+            std::make_shared<spdlog::sinks::basic_file_sink_st>("Game.log", true));
+
+        logger = std::make_shared<spdlog::logger>("Mineblocks", std::begin(logSinks),
+                                                        std::end(logSinks));
+
+        spdlog::register_logger(logger);
+
+        logger->set_level(spdlog::level::trace);
+    }
+
+    Logger Log::logger;
+
+    auto Log::GetLogger() -> Logger&
+    {
+        return logger;
+    }
+} // namespace Mineblocks
--- src/Mineblocks/Common/Log.hpp
+++ src/Mineblocks/Common/Log.hpp
@@ -2,25 +2,91 @@
 
 #include <spdlog/spdlog.h>
 
-namespace Mineblocks{
-    const char LogMsgPrefix[] = "[{}] ";
+namespace Mineblocks {
+    enum class LogCategory { Common, Core };
 
-    enum class LogCategory{
-        Common,
-        Core
+    auto LogCategoryToString(LogCategory category) -> const char*;
+
+    class Logger {
+      public:
+        Logger();
+
+        template <typename T, typename... Args>
+        void Log(spdlog::level::level_enum level, LogCategory category, const T& msg, Args&&... args){
+            std::string format =
+                fmt::format("[{}] {}", LogCategoryToString(category), msg);
+            logger->template log(level, format, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Trace(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::trace, category, msg, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Debug(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::debug, category, msg, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Info(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::info, category, msg, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Warn(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::warn, category, msg, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Error(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::err, category, msg, std::forward<Args>(args)...);
+        }
+
+        template <typename T, typename... Args>
+        void Critical(LogCategory category, const T& msg, Args&&... args)
+        {
+            Log(spdlog::level::critical, category, msg, std::forward<Args>(args)...);
+        }
+
+      private:
+        std::shared_ptr<spdlog::logger> logger;
     };
 
     class Log{
       public:
-        static void Initialize();
-
-        static auto Common() -> std::shared_ptr<spdlog::logger>;
-        static auto Core() -> std::shared_ptr<spdlog::logger>;
-
-        static auto LogCategoryToString(LogCategory category) -> const char*;
+        static auto GetLogger() -> Logger&;
 
       private:
-        static std::shared_ptr<spdlog::logger> commonLogger;
-        static std::shared_ptr<spdlog::logger> coreLogger;
+        static Logger logger;
     };
-}
+} // namespace Mineblocks
+
+
+#ifdef CONFIGURATION_DEBUG
+#define LOG_DEBUG_TRACE(...) ::Mineblocks::Log::GetLogger.Trace(__VA_ARGS__)
+#define LOG_DEBUG_DEBUG(...) ::Mineblocks::Log::GetLogger.Debug(__VA_ARGS__)
+#define LOG_DEBUG_INFO(...) ::Mineblocks::Log::GetLogger.Info(__VA_ARGS__)
+#define LOG_DEBUG_WARN(...) ::Mineblocks::Log::GetLogger.Warn(__VA_ARGS__)
+#define LOG_DEBUG_ERROR(...) ::Mineblocks::Log::GetLogger.Error(__VA_ARGS__)
+#define LOG_DEBUG_CRITICAL(...) ::Mineblocks::Log::GetLogger.Critical(__VA_ARGS__)
+#else
+#define LOG_DEBUG_TRACE(...)
+#define LOG_DEBUG_DEBUG(...)
+#define LOG_DEBUG_INFO(...)
+#define LOG_DEBUG_WARN(...)
+#define LOG_DEBUG_ERROR(...)
+#define LOG_DEBUG_CRITICAL(...)
+#endif
+
+#define LOG_TRACE(...) ::Mineblocks::Log::GetLogger.Trace(__VA_ARGS__)
+#define LOG_DEBUG(...) ::Mineblocks::Log::GetLogger.Debug(__VA_ARGS__)
+#define LOG_INFO(...) ::Mineblocks::Log::GetLogger.Info(__VA_ARGS__)
+#define LOG_WARN(...) ::Mineblocks::Log::GetLogger.Warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::Mineblocks::Log::GetLogger.Error(__VA_ARGS__)
+#define LOG_CRITICAL(...) ::Mineblocks::Log::GetLogger.Critical(__VA_ARGS__)
